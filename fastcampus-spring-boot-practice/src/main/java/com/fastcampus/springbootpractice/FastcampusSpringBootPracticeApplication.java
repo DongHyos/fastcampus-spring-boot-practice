b17bb89e065ae13f3900991dacc8240000b42c7a
DongHyos <dong87070@naver.com>
2022-12-25 22:48:30 +0900

    vault
    vault 학습

--- fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -2,33 +2,46 @@ package com.fastcampus.springbootpractice;
 
 import com.fastcampus.springbootpractice.properties.Myproperties;
 import com.fastcampus.springbootpractice.service.StudentService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.event.EventListener;
 
-@RequiredArgsConstructor
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
     private final Myproperties myproperties;
     private final StudentService studentService;
+    private final String username;
+    private final String password;
+
+    public FastcampusSpringBootPracticeApplication(
+            Myproperties myproperties,
+            StudentService studentService,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        this.myproperties = myproperties;
+        this.studentService = studentService;
+        this.username = username;
+        this.password = password;
+    }
 
 
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
-
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
 //        System.out.println("[configurationProps] " + myproperties.getHeight());
-        studentService.printStudent("jack");
-        studentService.printStudent("cassie");
-        studentService.printStudent("fred");
+//        studentService.printStudent("jack");
+//        studentService.printStudent("cassie");
+//        studentService.printStudent("fred");
+        System.out.println("id " + username);
+        System.out.println("pw " + password);
     }
 
 }
