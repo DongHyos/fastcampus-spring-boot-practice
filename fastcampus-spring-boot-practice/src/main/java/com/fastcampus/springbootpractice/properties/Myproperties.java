b5221d0245f50a269f546bb743e38e624f2cd587
DongHyos <dong87070@naver.com>
2022-12-25 18:40:42 +0900

    spring-boot-configuration-processor
    spring-boot-configuration-processor에 대해서 학습했다.

--- fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/properties/Myproperties.java
+++ fastcampus-spring-boot-practice/src/main/java/com/fastcampus/springbootpractice/properties/Myproperties.java
@@ -1,20 +1,19 @@
 package com.fastcampus.springbootpractice.properties;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+@Getter
 @ConstructorBinding
 @ConfigurationProperties("my")
 public class Myproperties {
 
+    /** 제 키에요 */
     private final Integer height;
 
     public Myproperties(Integer height) {
         this.height = height;
-    }
-
-    public Integer getHeight() {
-        return height;
     }
 
 }
